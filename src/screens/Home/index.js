1a12ec8cbbfdc26bdc220ba95ef7a9d9e6ab4130
Allan Camargo <allan.cdev@gmail.com>
2019-12-12 11:34:46 -0300

    add multer

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import {StatusBar, SafeAreaView, Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {List, Checkbox} from 'react-native-paper';
 
-const Page = () => {
-    return (
-        <SafeAreaView>
-            <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-            <Text>...</Text>
-        </SafeAreaView>
-    );
-};
+class Home extends React.Component {
+    state = {
+        expanded: true,
+    };
 
-export default Page;
+    _handlePress = () =>
+        this.setState({
+            expanded: !this.state.expanded,
+        });
+
+    render() {
+        return (
+            <SafeAreaView>
+                <List.Section title="ADMINISTRATIVO">
+                    <List.Accordion
+                        title="Documentos administrativos"
+                        left={props => <Icon {...props} icon="folder" />}>
+                        <List.Item title="Carta - carta teste" />
+                        <List.Item title="ANAC - Site ANAC" />
+                    </List.Accordion>
+                </List.Section>
+
+                <List.Section title="OPERACIONAL">
+                    <List.Accordion
+                        title="Categoria Allan"
+                        left={props => <List.Icon {...props} icon="folder" />}>
+                        <List.Item title="First item" />
+                        <List.Item title="Second item" />
+                    </List.Accordion>
+                </List.Section>
+            </SafeAreaView>
+        );
+    }
+}
+
+export default Home;
